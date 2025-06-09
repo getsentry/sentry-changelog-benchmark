5d061d4e21493b287f66e0cb17877c6d471ab800
David Cramer <dcramer@gmail.com>
2025-06-09 15:06:45 -0700

    Remove hast package (only used for type)

--- src/app/changelog/page.tsx
+++ src/app/changelog/page.tsx
@@ -1,53 +1,54 @@
-import {Fragment} from 'react';
-import type {Metadata} from 'next';
-import {serialize} from 'next-mdx-remote/serialize';
+import { Fragment } from "react";
+import type { Metadata } from "next";
+import { serialize } from "next-mdx-remote/serialize";
 
-import Header from './header';
-import {getChangelogs} from '../../server/utils';
-import {ChangelogEntry, ChangelogList} from '@/client/components/list';
-import {startSpan} from '@sentry/nextjs';
-import type {Plugin} from 'unified';
-import {visit} from 'unist-util-visit';
-import {Element} from 'hast';
+import Header from "./header";
+import { getChangelogs } from "../../server/utils";
+import { ChangelogEntry, ChangelogList } from "@/client/components/list";
+import { startSpan } from "@sentry/nextjs";
+import type { Plugin } from "unified";
+import { visit } from "unist-util-visit";
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
 export default async function Page() {
   const changelogs = await getChangelogs();
 
-  const changelogsWithPublishedAt = changelogs.filter(changelog => {
+  const changelogsWithPublishedAt = changelogs.filter((changelog) => {
     return changelog.publishedAt !== null;
   });
 
   const changelogsWithMdxSummaries = await startSpan(
-    {name: 'serialize changelog summaries'},
+    { name: "serialize changelog summaries" },
     () =>
       Promise.all(
-        changelogsWithPublishedAt.map(async (changelog): Promise<ChangelogEntry> => {
-          const mdxSummary = await serialize(
-            changelog.summary || '',
-            {
-              mdxOptions: {
-                rehypePlugins: [
-                  // Because we render the changelog entries as <a> tags, and it is not allowed to render <a> tags
-                  // within other a tags, we need to strip away the <a> tags inside the previews here.
-                  // @ts-ignore
-                  stripLinks,
-                ],
+        changelogsWithPublishedAt.map(
+          async (changelog): Promise<ChangelogEntry> => {
+            const mdxSummary = await serialize(
+              changelog.summary || "",
+              {
+                mdxOptions: {
+                  rehypePlugins: [
+                    // Because we render the changelog entries as <a> tags, and it is not allowed to render <a> tags
+                    // within other a tags, we need to strip away the <a> tags inside the previews here.
+                    // @ts-ignore
+                    stripLinks,
+                  ],
+                },
               },
-            },
-            true
-          );
-          return {
-            id: changelog.id,
-            title: changelog.title,
-            slug: changelog.slug,
-            // Because `getChangelogs` is cached, it sometimes returns its results serialized and sometimes not. Therefore we have to deserialize the string to be able to call toUTCString().
-            publishedAt: new Date(changelog.publishedAt!).toUTCString(),
-            categories: changelog.categories,
-            mdxSummary,
-          };
-        })
+              true
+            );
+            return {
+              id: changelog.id,
+              title: changelog.title,
+              slug: changelog.slug,
+              // Because `getChangelogs` is cached, it sometimes returns its results serialized and sometimes not. Therefore we have to deserialize the string to be able to call toUTCString().
+              publishedAt: new Date(changelog.publishedAt!).toUTCString(),
+              categories: changelog.categories,
+              mdxSummary,
+            };
+          }
+        )
       )
   );
 
@@ -62,7 +63,7 @@ export default async function Page() {
 export function generateMetadata(): Metadata {
   return {
     description:
-      'Stay up to date on everything big and small, from product updates to SDK changes with the Sentry Changelog.',
+      "Stay up to date on everything big and small, from product updates to SDK changes with the Sentry Changelog.",
     alternates: {
       canonical: `https://sentry.io/changelog/`,
     },
@@ -70,10 +71,10 @@ export function generateMetadata(): Metadata {
 }
 
 const stripLinks: Plugin = () => {
-  return tree => {
-    return visit(tree, 'element', (node: Element) => {
-      if (node.tagName === 'a') {
-        node.tagName = 'span';
+  return (tree) => {
+    return visit(tree, "element", (node: any) => {
+      if (node.tagName === "a") {
+        node.tagName = "span";
         if (node.properties) {
           delete node.properties.href;
           delete node.properties.class;
